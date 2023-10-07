62263da0191144aa21bb832bd8fc0d579ceff563
IVadymMarchenko <topim31@gmail.com>
2023-10-07 14:33:40 +0300

    iter_dir_home_work

---
+++ practic.py
@@ -0,0 +1,45 @@
+import re
+import pathlib
+import string
+
+
+def normalize(string_):
+    transliteration_dict = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g',
+        'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh',
+        'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l',
+        'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+        'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts',
+        'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '',
+        'э': 'eh', 'ю': 'yu', 'я': 'ya',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G',
+        'Д': 'D', 'Е': 'E', 'Ё': 'Yo', 'Ж': 'Zh',
+        'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L',
+        'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R',
+        'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts',
+        'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Sch', 'Ъ': '', 'Ы': 'Y', 'Ь': '',
+        'Э': 'Eh', 'Ю': 'Yu', 'Я': 'Ya',
+    }
+    c = string_.rfind('.')
+    d = string_[:c]
+    new_string = ''
+    for i in d:
+        if i in transliteration_dict.keys():
+            new_string += transliteration_dict[i]
+        elif i.isdigit():
+            new_string += i
+        elif i in string.ascii_letters:
+            new_string += i
+        elif i == '.':
+            new_string+='_'
+        else:
+            new_string+='_'
+    new_string+=string_[c:]
+    return new_string
+
+
+print(normalize('афA.%7^Ра57?юю.mp4'))
+
+
+
+
